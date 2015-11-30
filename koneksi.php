d3caa08697d0c4690680eaaca9798defeeef04ed
Apranta <Reziapriliansyah@gmail.com>
2015-11-30 15:55:42 +0700

    Update koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -1,8 +1,8 @@
 <?php
-$host = "lbr-cdbr-azure-south-a.cloudapp.net";
+$host = "br-cdbr-azure-south-a.cloudapp.net";
 $user = "b67515a75fa4a1";
-$pass = "2ce8a2bf";
-$db = "tubes";
+$pass = "bd9a8d78";
+$db = "rezidb";
 $konek = mysql_connect($host, $user, $pass) or die ('Koneksi Gagal! ');
 mysql_select_db($db);
 ?>
