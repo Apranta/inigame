8b54443afeb8c14b9a3d7f82ddb9450a679bf67a
Rezi Apriliansyah <Rezi Apriliansyah>
2015-11-30 15:34:00 +0700

    koneksi

--- koneksi.php
+++ koneksi.php
@@ -1,7 +1,7 @@
 <?php
-$host = "localhost";
-$user = "root";
-$pass = "";
+$host = "lbr-cdbr-azure-south-a.cloudapp.net";
+$user = "b67515a75fa4a1";
+$pass = "2ce8a2bf";
 $db = "tubes";
 $konek = mysql_connect($host, $user, $pass) or die ('Koneksi Gagal! ');
 mysql_select_db($db);
